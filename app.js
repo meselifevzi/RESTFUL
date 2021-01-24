4a5006488d846400d0dd17e051701971afaddfa1
Fevzi Meşeli <fevzimeseli1702@gmail.com>
2021-01-24 14:33:31 +0300

    Update DB

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-require('dotenv/config')
+require('dotenv').config()
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
--- routes/posts.js
+++ routes/posts.js
@@ -1,19 +1,17 @@
 const express =require('express');
-const router =express.Router()
+const router =express.Router();
 const Post = require('../models/Post');
 
 //GET BACK ALL THE POSTS
-router.get('/', (req,res) => {
+router.get('/', async (req, res) => {
     try {
         const posts = await Post.find()
         res.json(posts)
-    } catch (err) {
+    }catch (err) {
         res.json({message: err})
     }
-})
-router.get('/specific', (req,res) => {
-    res.send('SPECIFIC POST')
-})
+}) 
+
 
 //SUMBIT A POST
 router.post('/', async (req,res) => {
